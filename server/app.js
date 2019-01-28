9d2e4e1a5a1ae4bfd4f0213d3b4bf9e23f27d248
Bhavay Anand <bhavayanandcse@gmail.com>
2019-01-28 16:00:53 +0530

    graphql schema and react added

--- server/app.js
+++ server/app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const app = express();
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+//allow cross origin requests
+app.use(cors());
+
+mongoose.connect("mongodb://bhavayAnand9:qazPLM2222@ds135800.mlab.com:35800/graphql-project")
+
+mongoose.connection.once('open', ()=>{
+	console.log('connected to db');
+});
 
 app.use('/graphql', graphqlHTTP({
 	//similar to schema:schema
@@ -9,6 +20,6 @@ app.use('/graphql', graphqlHTTP({
 	graphiql: true
 }));
 
-app.listen(3000, ()=>{
-	console.log('port 3000');
+app.listen(4000, ()=>{
+	console.log('port 4000');
 })
